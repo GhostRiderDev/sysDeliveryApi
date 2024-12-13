af99fe60365b52c00cb66042e90d768e2fb4976e
ghostRiderDev <olvadis2004@gmail.com>
2024-12-12 20:02:12 -0500

    added user model

---
+++ src/authService/src/domain/user/user.go
@@ -0,0 +1,35 @@
+package user
+
+import (
+	"errors"
+	"time"
+)
+
+type UserRole string
+
+const (
+	ADMIN   UserRole = "ADMIN"
+	CLIENT  UserRole = "CLIENT"
+	DELIVER UserRole = "DELIVER"
+)
+
+type User struct {
+	id       string
+	username string
+	email    string
+	name     string
+	status   bool
+	role     UserRole
+	password string
+	createdAt time.Time
+	updatedAt time.Time
+}
+
+func (r UserRole) isValidRole() error {
+	switch r {
+	case ADMIN, CLIENT, DELIVER:
+		return nil
+	}
+
+	return errors.New("Invalod Userrole")
+}
